10608eb837f3c25a6e929fa094d99e288076ce0f
Althor05 <albertovelascotes05@gmail.com>
2025-03-26 19:52:55 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <map>
+#include <algorithm>
 
 bool comprobar(const std::string& a, const std::string sub[3], int i, int d) {
     std::string suba = a.substr(i, d - i + 1);
@@ -13,43 +15,45 @@ bool comprobar(const std::string& a, const std::string sub[3], int i, int d) {
         (suba == sub[2] + sub[1]);
 }
 
+
 void dyv(const std::string& A, const std::string sub[3], int i, int d,
-    std::set<int>& sol
-) {
-    int len = (d - i + 1); // longitud de subcadena
+    std::set<int>& sol)
+{
+    //longitud de cadena
+    int len = (d - i + 1);
+    // caso base
     if (len < 6) {
+    // caso base
     } else if (len == 6) {
-        if (comprobar(A, sub, i, d))
-           sol.insert(i);
+         if (comprobar(A, sub, i, d))
+            sol.insert(i);
     } else {
+        // dividir
         dyv(A, sub, i, i + ((len/2) - 1), sol); // mitad izq
         dyv(A, sub, i + (len/2), d, sol); // mitad dcha
-        for (int h = std::max(i + ((len/2) - 5), 0); 
-                h + 5 <= std::min(i + ((len/2) + 4), d); h++)
+        // combinar
+        for (int h = std::max(i + ((len/2) - 5), i); h + 5 <= std::min(i + ((len/2) + 4), d); h++)
             if (comprobar(A, sub, h, h + 5))
                 sol.insert(h);
     }
 }
 
+
 int main(int argc, char **argv) {
+    //Cadena principal
     std::string A;
     std::cin >> A;
-
+    //Subcadenas
     std::string sub[3] = {"acb", "aac", "dca"};
-    
+    //Solución
     std::set<int> sol;
 
-
-    if (argc == 2 && std::string(argv[1]) == "-dyv")
+   
         dyv(A, sub, 0, A.length() - 1, sol);
-    if (argc == 2 && std::string(argv[1]) == "-dir")
-        for (int i = 0; i < A.length() - 6; i++)
-            if (comprobar(A, sub, i, i + 5))
-                sol.insert(i);
-    
 
-    for (int s : sol) 
+    printf("sol: %ld {", sol.size());
+    for (int s : sol)
         printf("%d, ", s + 1);
-    printf("\nsol: %ld\n", sol.size());
+    printf("}\n");
 }
 
